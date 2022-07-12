274a71cced0e544a1cde497dca95516d39e3bb08
Kairos22 <alandiko84@gmail.com>
2022-07-12 14:00:22 +0200

    new version

--- solution.js
+++ solution.js
@@ -1,9 +1,9 @@
-let number = 15;
-if (number % 3 === 0 && number % 5 === 0) {
+let number = 3;
+if (number % 3 == 0 && number % 5 == 0) {
     console.log('FizzBuzz');
-} else if (number % 3 === 0) {
+} else if (number % 3 == 0) {
     console.log('Fizz');
-} else if (number % 5 === 0) {
+} else if (number % 5 == 0) {
     console.log('Buzz');
 } else {
     console.log('number');
